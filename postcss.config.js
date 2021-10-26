940c34022d53d232b54f9da60c6fa3eaf82ea750
ReicaD <dbamanya447@gmail.com>
2021-10-26 10:06:35 -0400

    Tailwind  Work

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// makingsure that the this.directives can be corresponding with the tail
+
+module.exports ={
+    plugins:[
+        require('tailwindcss'),
+        require('autoprefixer'),
+
+    ]     
+}
